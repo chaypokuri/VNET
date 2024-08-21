42297a51b33c38783363f2ba630a81006e91bbfc
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-08-21 18:16:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "azurerm" {
   features {}
-subscription_id = "c2bd123a-183f-43d5-bf41-c725494e595a"
-tenant_id = "3180c264-31bc-4113-8f50-b7393a40457b"
-client_id = "1a046c02-8c39-4f1d-b30b-93f41a9c6b15"
-client_secret = "kUz8Q~qwom0J-MM5ZNqexXyUOguygMj5QELdhdl5"
+  subscription_id = "c2bd123a-183f-43d5-bf41-c725494e595a"
+  tenant_id       = "3180c264-31bc-4113-8f50-b7393a40457b"
+  client_id       = "1a046c02-8c39-4f1d-b30b-93f41a9c6b15"
+  client_secret   = "kUz8Q~qwom0J-MM5ZNqexXyUOguygMj5QELdhdl5"
 }
-
+ 
 terraform {
   required_providers {
     azurerm = {
@@ -14,50 +14,63 @@ terraform {
     }
   }
 }
+ 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
-
+ 
 resource "azurerm_app_service_plan" "example" {
   name                = "example-appserviceplan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-
+ 
   sku {
     tier = "Standard"
     size = "S1"
   }
 }
-
-resource "azurerm_app_service" "example" {
-  name                = "example-app-service"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
-}
+ 
 resource "azurerm_virtual_network" "example" {
   name                = "example-virtual-network"
   address_space       = ["10.0.0.0/24"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
+ 
 resource "azurerm_subnet" "example" {
   name                 = "example-subnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.1.0/27"]
-
+ 
   delegation {
     name = "example-delegation"
-
+ 
     service_delegation {
       name    = "Microsoft.Web/serverFarms"
       actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
     }
   }
 }
+ 
+resource "azurerm_app_service" "example" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+ 
+  site_config {
+    vnet_route_all_enabled = true  # Ensures VNet integration is enabled
+  }
+ 
+  identity {
+    type = "SystemAssigned"
+  }
+}
+ 
 resource "azurerm_app_service_virtual_network_swift_connection" "example" {
   app_service_id = azurerm_app_service.example.id
   subnet_id      = azurerm_subnet.example.id
 }
+
